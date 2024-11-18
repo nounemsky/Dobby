63172d5ef96b281d55f0bb927fcd15e23c88924b
Verone <sherlmori14@duck.com>
2024-11-18 16:50:44 +0000

    spotify.py

--- spotify.py
+++ spotify.py
@@ -4,47 +4,36 @@ import time
 import comtypes
 
 def initialize_com():
-    """Инициализирует COM для текущего потока."""
     comtypes.CoInitialize()
 
 def uninitialize_com():
-    """Деинициализирует COM для текущего потока."""
     comtypes.CoUninitialize()
 
-# --- Основные функции управления Spotify ---
-
 def open_spotify():
-    """Открывает Spotify и переводит в полноэкранный режим."""
-    subprocess.Popen(['путь к приложению Spotify'])
-    time.sleep(5)  # Подождите, пока приложение откроется
-    pyautogui.hotkey('f11')  # Нажатие F11 для полноэкранного режима
-    return "Spotify открыт в полноэкранном режиме."
+    subprocess.Popen(['ПУТЬ к приложению Spotify'])
+    time.sleep(5) 
+    pyautogui.hotkey('f11')
+    return 
 
 def play_music():
-    """Возобновляет воспроизведение музыки."""
     pyautogui.press('playpause')  
-    return "Музыка возобновлена."
+    return 
 
 def pause_music():
-    """Приостанавливает воспроизведение музыки."""
     pyautogui.press('playpause')  
-    return "Музыка приостановлена."
+    return 
 
 def skip_track():
-    """Переключает на следующий трек."""
     pyautogui.press('nexttrack')  
-    return "Переключился на следующий трек."
+    return 
 
 def previous_track():
-    """Переключает на предыдущий трек."""
     pyautogui.press('prevtrack')  
-    return "Переключился на предыдущий трек."
+    return
 
-# Инициализация COM в основном потоке
 if __name__ == "__main__":
     initialize_com()
     try:
-        
         pass  
     finally:
         uninitialize_com()
